d519cb1c83eecc7f75705efa43176a3e81788abd
Kevin Dai <daikevin19@yahoo.com>
2019-05-14 23:37:02 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School # 'model' in the domain model
+  attr_accessor :roster, :name, :grade
+  
+  GRADE = []
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade) # iterates per instance
+    @roster[grade] ||= [] # why is this correct and not just = ?
+    @roster[grade] << name
+  end
+  
+  def grade(grade_number) 
+    @roster[grade_number]
+  end
+  
+  def sort 
+  sorted = {}    
+    @roster.each do |grade, names|
+      sorted[grade] = names.sort 
+    end
+    sorted
+  end
+end
